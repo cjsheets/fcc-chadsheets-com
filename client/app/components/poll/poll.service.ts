4e95f9bc297cb40b986c218043b9d17b04a25c21
Chad Sheets <cjsheets@gmail.com>
2016-12-02 13:25:57 -0800

    added poll service

--- client/app/components/poll/poll.service.ts
+++ client/app/components/poll/poll.service.ts
@@ -1,25 +1,54 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
 
-import 'rxjs/add/operator/toPromise';
+export class Poll {
+  constructor(
+    public id: number,
+    public title: string,
+    public description: string,
+    public options: [PollOption]
+  ){}
+}
+
+export class PollOption {
+  constructor(
+    public id: number,
+    public option: string,
+    public votes: number
+  ){}
+}
+
+/* temporary poll provider */
+let POLLS = [
+  new Poll(
+    0, 'Chicken or Egg',
+    'One had to come first',
+    [new PollOption(0, 'Chicken', 0),
+     new PollOption(1, 'Egg', 0)]),
+  new Poll(
+    1,
+    'Why did the Chicken Cross Road',
+    'Choose your favorite reason',
+    [new PollOption(0, 'Get to the other side', 0),
+     new PollOption(1, 'Flee from a wolf', 0),
+     new PollOption(2, 'Prove it wasn\'t chicken', 0)]),
+  new Poll(
+    2,
+    'What is your favorite color',
+    'Choose your most prefered color below',
+    [new PollOption(0, 'Red', 0),
+     new PollOption(1, 'Green', 0),
+     new PollOption(2, 'Blue', 0)])
+];
+let pollsPromise = Promise.resolve(POLLS);
 
 @Injectable()
 export class PollService {
-
-  private headers = new Headers({'Content-Type': 'application/json'});
-
-  constructor(private http: Http) { }
-
-  getImageResults(searchQuery: string): Promise<[{}]> {
-      console.log(searchQuery);
-      return this.http.get(searchQuery)
-        .toPromise()
-        .then(response => response.json())
-        .catch(this.handleError);
+  getPolls(): Promise<[Poll]> {
+    return pollsPromise;
   }
 
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred - image-search.service', error); // Needs to be improved
-    return Promise.reject(error.message || error);
+  getPoll(id: number | string): Promise<Poll> {
+    return pollsPromise
+      .then(polls => polls.find(poll => poll.id === +id));
   }
- }
+}
